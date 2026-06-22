3b9dcb3bd2a98483ddbcdd9e55566a06886b728d
emile <emuller2607@gmail.com>
2026-06-22 11:40:59 +0200

    Add extra section in footer with icons.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import FooterIcons from "./FooterIcons.jsx";
 
 function Footer(props) {
     return (
@@ -6,10 +7,14 @@ function Footer(props) {
         <footer
             className="font-montserrat text-center"
         >
-            <div>
-
-            </div>
-            <h6>
+            <div className="bg-gray-950 py-8" >
+                <FooterIcons />
+                <h5 className="text-2xl font-bold text-blue-900 pt-4">Culture House</h5>
+                <p className="text-white py-3">Audio • Video • Podcast</p>
+                <p className="text-white py-3">Studio en op locatie</p>
+                <p className="text-white py-3">info@culturehouse.tv</p>
+           </div>
+            <h6 className="py-4 px-4">
                 © 2026 Culture House. Alle rechten voorbehouden.
             </h6>
         </footer>
